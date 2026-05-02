72b117e271a00b97d7a7928220d8d5712c1cbb0e
Rachid Mouradi <rm109@alu.ua.es>
2026-05-02 10:44:01 +0200

    mejoras en https - genera certificados nuevos en cada ejecución

--- pkg/certgen/certgen.go
+++ pkg/certgen/certgen.go
@@ -7,29 +7,34 @@ import (
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/pem"
+	"fmt"
 	"math/big"
 	"net"
+	"os"
 	"time"
 )
 
 // Generate crea un certificado X.509 autofirmado con clave ECDSA P-256.
-// El certificado es válido para localhost y 127.0.0.1 durante 1 año.
-// Devuelve el certificado y la clave privada en formato PEM.
-func Generate() (certPEM, keyPEM []byte, err error) {
+// El certificado se guarda en disco y es válido para localhost y 127.0.0.1.
+// Se genera uno nuevo en cada ejecución para que siempre cambie.
+func Generate(certPath, keyPath string) error {
 	priv, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	if err != nil {
-		return nil, nil, err
+		return err
 	}
 	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
 	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
 	if err != nil {
-		return nil, nil, err
+		return err
 	}
+
+	// Generamos un identificador aleatorio para la organización para que cambie visiblemente
+	randOrg := fmt.Sprintf("Ya tu sabes Dev - %x", serialNumber.Bytes()[:4])
 
 	template := &x509.Certificate{
 		SerialNumber: serialNumber,
 		Subject: pkix.Name{
-			Organization: []string{"Ya tu sabes Dev"},
+			Organization: []string{randOrg},
 			CommonName:   "localhost",
 		},
 		DNSNames:              []string{"localhost"},
@@ -44,16 +49,22 @@ func Generate() (certPEM, keyPEM []byte, err error) {
 
 	certDER, err := x509.CreateCertificate(rand.Reader, template, template, &priv.PublicKey, priv)
 	if err != nil {
-		return nil, nil, err
+		return err
 	}
 
-	certPEM = pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+	if err := os.WriteFile(certPath, certPEM, 0644); err != nil {
+		return err
+	}
 
 	privDER, err := x509.MarshalECPrivateKey(priv)
 	if err != nil {
-		return nil, nil, err
+		return err
 	}
-	keyPEM = pem.EncodeToMemory(&pem.Block{Type: "EC PRIVATE KEY", Bytes: privDER})
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "EC PRIVATE KEY", Bytes: privDER})
+	if err := os.WriteFile(keyPath, keyPEM, 0600); err != nil {
+		return err
+	}
 
-	return certPEM, keyPEM, nil
+	return nil
 }
